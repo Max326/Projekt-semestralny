d8f5a4ebd9e7b05737a6c3ff425bfaf133753ddb
Max326 <maxconnor326@gmail.com>
2024-01-05 00:50:58 +0100

    can walk next to walls

--- src/KeyboardHandler.h
+++ src/KeyboardHandler.h
@@ -16,24 +16,24 @@ public:
 		restX = head.GetPosX() % head.bodySize;
 		restY = head.GetPosY() % head.bodySize;
 
-		if(IsKeyDown(KEY_W) && head.GetDirY() != 1 && head.GetDirY() != -1) {
-			head.SetPosX(head.GetPosX() - restX + head.bodySize / 2);
+		if(IsKeyDown(KEY_W) && head.GetDirY() != 1 && head.GetDirY() != -1 && !IsKeyDown(KEY_A) && !IsKeyDown(KEY_D)) {
+			head.SetPosX(head.GetPosX() - restX);
 			head.GoUp();
 		};
-		if(IsKeyDown(KEY_S) && head.GetDirY() != -1 && head.GetDirY() != 1) {
-			head.SetPosX(head.GetPosX() - restX + head.bodySize / 2);
+		if(IsKeyDown(KEY_S) && head.GetDirY() != -1 && head.GetDirY() != 1 && !IsKeyDown(KEY_A) && !IsKeyDown(KEY_D)) {
+			head.SetPosX(head.GetPosX() - restX);
 			head.GoDown();
 		};
 
-		if(IsKeyDown(KEY_D) && head.GetDirX() != -1 && head.GetDirX() != 1) {
-			head.SetPosY(head.GetPosY() - restY + head.bodySize / 2);
+		if(IsKeyDown(KEY_D) && head.GetDirX() != -1 && head.GetDirX() != 1 && !IsKeyDown(KEY_W) && !IsKeyDown(KEY_S)) {
+			head.SetPosY(head.GetPosY() - restY);
 			head.GoRight();
 		};
-		if(IsKeyDown(KEY_A) && head.GetDirX() != 1 && head.GetDirX() != -1) {
-			head.SetPosY(head.GetPosY() - restY + head.bodySize / 2);
+		if(IsKeyDown(KEY_A) && head.GetDirX() != 1 && head.GetDirX() != -1 && !IsKeyDown(KEY_W) && !IsKeyDown(KEY_S)) {
+			head.SetPosY(head.GetPosY() - restY);
 			head.GoLeft();
 		};
-		// TODO snake stops when W and D pressed and similar
+		// TODO how to skip to the next square and not let the user stay in one?
 	}
 
 public:
--- src/main.cpp
+++ src/main.cpp
@@ -9,44 +9,39 @@ int main() {
 	const int screenWidth = 800;
 	const int screenHeight = 600;
 
-	// int ball_x = 100;
-	// int ball_y = 100;
-
-	// int ball_speed_x = 5;
-	// int ball_speed_y = 0;
-
 	float timeScale = 0.5;
 
 	Head head;
 	KeyboardHandler keyboardHandler(head);
 
-	head.SetSpeed(3);
+	head.SetSpeed(2);
 
-	head.SetPosX(screenWidth/2);
-	head.SetPosY(screenHeight/2);
+	head.SetPosX(screenWidth / 2);
+	head.SetPosY(screenHeight / 2);
 
 	head.SetDirX(1);
 	head.SetDirY(0);
 
 	head.bodySize = 20;
 
+	int columns = screenWidth / head.bodySize;
+	int rows = screenHeight / head.bodySize;
+
 	InitWindow(screenWidth, screenHeight, "Snake");
-	SetTargetFPS(144);
+	SetTargetFPS(60);
 
 	while(WindowShouldClose() == false) {
 		// float deltaTime = GetFrameTime() * timeScale;
 
 		BeginDrawing();
-		ClearBackground(BLACK);
+		ClearBackground(DARKGRAY);
 
-		if(head.GetPosX() >= screenWidth - head.bodySize / 2 || head.GetPosX() <= head.bodySize / 2) {
-			// ball_speed_x *= -1;
+		if(head.GetPosX() > screenWidth - head.bodySize || head.GetPosX() < 0) {
 			std::cout << "end of field x" << std::endl;
 			head.SetDirX(-head.GetDirX());
 			break;
 		}
-		if(head.GetPosY() >= screenHeight - head.bodySize / 2 || head.GetPosY() <= head.bodySize / 2) {
-			// ball_speed_y *= -1;
+		if(head.GetPosY() > screenHeight - head.bodySize || head.GetPosY() < 0) {
 			head.SetDirY(-head.GetDirY());
 			std::cout << "end of field y" << std::endl;
 			break;
@@ -57,21 +52,19 @@ int main() {
 			break;
 		}
 		if(IsKeyDown(KEY_F)) {
-			// ball_speed_x = 0;
-			// ball_speed_y = 0;
-
 			head.SetSpeed(0);
 		}
 		head.Move();
 
 		std::cout << "headposx: " << head.GetPosX() << " headposy: " << head.GetPosY() << "\n";
 
-		// ball_x += ball_speed_x;
-		// ball_y += ball_speed_y;
-
 		DrawRectangle(head.GetPosX(), head.GetPosY(), head.bodySize, head.bodySize, GREEN);
 
-		DrawGrid(1000, 100);
+		for(int i = 0; i < columns; i++) {
+			for(int j = 0; j < rows; j++) {
+				DrawRectangleLines(i * head.bodySize, j * head.bodySize, head.bodySize, head.bodySize, GRAY);
+			}
+		}
 
 		EndDrawing();
 
