faed32f90407f79df70887fb4cfc5be8e4880393
Max326 <maxconnor326@gmail.com>
2024-01-06 16:08:24 +0100

    ąąąąąąąąąąąąąąąą

--- src/Body.h
+++ src/Body.h
@@ -1,8 +1,20 @@
 #pragma once
 
+#include <vector>
+#include <iostream>
+
 #include "Block.h"
 
 class Body : public Block
 {
-    
+    std::vector<Block> bodyBlocks;
+
+    // TODO: implement a list of snake elements by the bodyblocks vector of vector2s
+    // TODO: make a queue of direction vectors change the positions of blocks (function for that)
+
+    void UpdateSnake(std::vector<Block> &bodyBlocks){
+        for(int i = 0; i < bodyBlocks.size(); i++){
+            std::cout << "dupa\n";   
+        }
+    }
 };
