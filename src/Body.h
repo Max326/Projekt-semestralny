a9b21beb8ffd19b5a601770426d310f1c5f72a26
Max326 <maxconnor326@gmail.com>
2024-01-07 20:45:51 +0100

    cleeenup

--- src/Body.h
+++ src/Body.h
@@ -17,8 +17,6 @@ public:
 public:
 	void UpdateSnake() {
 		if(bodyBlocks.size() > 1) {
-			// std::cout << "poczatek bodybloksa\n";
-
 			auto it1 = bodyBlocks.rbegin();
 
 			while(std::next(it1) != bodyBlocks.rend()) {
